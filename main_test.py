90b7412b3bcc9ba5ad7eebd1a930e4bbb0b39a42
Azimjon Sobirov <sobirovazimjon02@gmail.com>
2025-08-21 16:22:28 +0800

    Wrote tests for 3 features

--- main_test.py
+++ main_test.py
@@ -1,1 +1,48 @@
-# TODO: To write tests without AI.
+import unittest
+from unittest.mock import MagicMock, patch
+import main
+
+
+class TestStart(unittest.TestCase):
+    
+    @patch("main.bot.send_message")
+    def test_start_handler(self, mock_message):
+        msg = MagicMock()
+        msg.chat.id = 90
+        msg.from_user.id = 90
+        main.users = {}
+        main.start(msg)
+        self.assertEqual(mock_message.call_count, 2)
+        welcome_text = mock_message.call_args_list[0][0][1]
+        self.assertIn("Hi! This bot lets you borrow books and read them.", welcome_text)
+
+
+
+class TestRegister(unittest.TestCase):
+
+    @patch("main.bot.send_message")
+    def test_register_feature(self,mock_message):
+        message = MagicMock()
+        message.chat.id = 809
+        message.from_user.id = 809
+        main.users = {}
+        main.register(message)
+        self.assertTrue(mock_message.called)
+        args, _ = mock_message.call_args
+        self.assertEqual(args[0], 809)
+        self.assertIn("Enter your name:", args[1])
+
+class TestBorrowHandler(unittest.TestCase):
+
+    @patch("main.bot.send_message")
+    def test_borrowing(self, mock_message):
+        message = MagicMock()
+        message.chat.id = 9090
+        message.from_user.id = 9090
+        main.users = {}
+        main.books = []
+        main.borrowing_book(message)
+        self.assertTrue(mock_message.called)
+        args, _ = mock_message.call_args
+        self.assertEqual(args[0], 9090)
+        self.assertIn("Enter the book ID or name to borrow: ", args[1])
